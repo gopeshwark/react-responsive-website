35d36892d9afae4b95b4c47a2c97433093fad0e4
Gopeshwar Kumawat <gopeshwark@gmail.com>
2020-08-30 12:17:43 +0530

    Intialized website with basic layout and created home, about, services, contact, navbar  pages and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Home from './Containers/Home/Home';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './Containers/Contact/Contact';
+import About from './Containers/About/About';
+import Services from './Containers/Services/Services';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/services" component={Services}/>
+        <Route exact path="/contact" component={Contact} />
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
 }
